d6e34ed8b56836c25d7303b46834a396ab532867
Lissamion <royssy@bk.ru>
2021-04-30 01:29:46 +0300

    fixed

--- in.js
+++ in.js
@@ -1,53 +1,32 @@
 $(function () {
-
   $('.setting-button').on('click', function () {
-    console.log('asd');
     $('.bg-green').toggleClass('bg-green--active')
   });
-
 });
 
+const a = document.getElementById('a');
+const b = document.getElementById('b');
 
-let a = document.getElementById('a');
-let b = document.getElementById('b');
-let sumM1 = document.getElementById('sum1');
-let sumM2 = document.getElementById('sum2');
-let sumM3 = document.getElementById('sum3');
-
-let inputa = document.getElementById('a');
-inputa.onkeydown = function (e) {
-  let aval = document.getElementById("a");
-  if (e.keyCode == 8 || e.keyCode == 46) {
-    aval.value = aval.value.substring(0, aval.value.length - 1);
-  }
-
-  return !(
-    /[^0-9.]$/.test(e.key)
-  );
+const inputA = document.getElementById('a');
+inputA.onkeydown = function (e) {
+  return (e.key >= '0' && e.key <= '9') || e.key == 'ArrowLeft' || e.key == 'ArrowRight' || e.key == 'Delete' || e.key == 'Backspace';
 }
 
-let inputb = document.getElementById('b');
-inputb.onkeydown = function (e) {
-  let aval = document.getElementById("b");
-  if (e.keyCode == 8 || e.keyCode == 46) {
-    aval.value = aval.value.substring(0, aval.value.length - 1);
-  }
-
-  return !(/[^0-9.]$/.test(e.key));
+const inputB = document.getElementById('b');
+inputB.onkeydown = function (e) {
+  return (e.key >= '0' && e.key <= '9') || e.key == 'ArrowLeft' || e.key == 'ArrowRight' || e.key == 'Delete' || e.key == 'Backspace';
 }
 
 document.getElementById('a').value = "";
 document.getElementById('b').value = "";
 
-let numberChecker = () => {
-  let regex = /^[-+]?\d+(\.\d+)?$/;
+function numberChecker() {
+  let val = a.value;
 
-  if (regex.test(a.value) && regex.test(b.value))
-    sumM1.innerText = (+a.value + (+a.value / 100 * +b.value)).toString();
-  let s1 = (+a.value + (+a.value / 100 * +b.value));
-  sumM2.innerText = (+s1 + (+s1 / 100 * +b.value)).toString();
-  let s2 = (+s1 + (+s1 / 100 * +b.value));
-  sumM3.innerText = (+s2 + (+s2 / 100 * +b.value)).toString();
+  for (var i = 0; i < 3; i++) {
+    $('td[name=sum]')[i].innerText = (+val + (+val / 100 * +b.value)).toString();
+    val = (+val + (+val / 100 * +b.value));
+  }
 }
 
 a.addEventListener("input", numberChecker);
